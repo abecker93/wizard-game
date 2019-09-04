ab5d67fbfb0884ffcef03094f363f9634cf918db
Andrew Becker <ajbecker@uvm.edu>
2019-09-04 12:02:34 -0400

    Finished first interaction with wizard

--- quinns_wiz.cpp
+++ quinns_wiz.cpp
@@ -13,16 +13,46 @@ int main(){
   char platter [12][50]={"platter", "Platter", "cheese","Cheese","onions","Onions","raw onions","Raw onions", "platter of raw onions", "Platter of raw onions", "platter of raw onions and cheese", "Platter of raw onions and cheese"};
   //initial output and input
   std::cout << "Due to the state of his robes, you can tell this wizard has fallen on hard times.\nWould you like to buy him a mug of ale, a heel of bread, or a platter of raw onions and cheese?" << '\n';
+  gift_input:
   std::cin >> gift;
   //compare gift to possible outputs
-  bool compares = false;
+  int compares = 2;
   int count = 0;
-  while (!compares && count<=6) {
-    count = count + 1;
-    string check = ales[count];
+  string check;
+  while (compares!=0 && count<6) {
+    check = ales[count];
     compares = gift.compare(check);
+    count = count + 1;
   };
-  if (compares) {
-    std::cout << "The wizard, without hesitation, starts drinking the ale, and asks 'how about a heel of bread to go with that ale?'" << '\n';
+  if (compares==0) {
+    std::cout << "The wizard, without hesitation, starts drinking the ale, and asks 'does this come with bread or perhaps a platter of onions and cheese?'" << '\n';
+    goto end_rude_wiz;
   };
+  compares = 2;
+  count = 0;
+  while (compares!=0 && count<6) {
+    check = heels[count];
+    compares = gift.compare(check);
+    count = count + 1;
+  };
+  if (compares==0) {
+    std::cout << "The wizard, without hesitation, starts eating the bread, and asks 'does this come with ale or perhaps a platter of onions and cheese?'" << '\n';
+    goto end_rude_wiz;
+  };
+  compares = 2;
+  count = 0;
+  while (compares!=0 && count<12) {
+    check = platter[count];
+    compares = gift.compare(check);
+    count = count + 1;
+  };
+  if (compares==0) {
+    std::cout << "The wizard, without hesitation, starts eating the platter, and asks 'does this come with bread or perhaps a mug of ale?'" << '\n';
+    goto end_rude_wiz;
+  };
+  std::cout << "The bartender didn't understand your order. Try again:\n";
+  goto gift_input;
+  end_rude_wiz:
+  std::cout << "End test\n";
+  //need more content
 };
